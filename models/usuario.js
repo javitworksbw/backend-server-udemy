0267ef70c29f09dfc017d371030c521c41ae98d7
Javier Bueno Santana <javitempab@gmail.com>
2019-08-14 23:51:11 +0200

    Commit Inicial - Seccion 10 - Hospital APP- Backend server

---
+++ models/usuario.js
@@ -0,0 +1,32 @@
+// modelo para la coleccion usuarios
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+    email: {
+        type: String,
+        required: [true, 'El correo es necesario'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es necesario']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        required: true,
+        default: 'USER_ROLE'
+    }
+
+});
+
+// User model based on userSchema
+module.exports = mongoose.model('User', userSchema);
+
